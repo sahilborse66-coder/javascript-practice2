d9d5fc5dbcd81e27699f723f4269be44b9ba4927
Sahil Borse <sahilborse66@gmail.com>
2026-08-08 18:53:32 +0530

    Added JavaScript practice

---
+++ script.js
@@ -0,0 +1,61 @@
+// console.log("Hello, World!");
+
+// let a=5;
+// let b=2;
+
+// console.log("a =" ,a,"& b =",b);
+// console.log("a + b =", a + b);
+// console.log("a - b =", a - b);
+// console.log("a * b =", a * b);
+// console.log("a / b =", a / b);
+// console.log("a % b =", a % b);
+// console.log("a ** b =", a ** b);
+
+
+
+// let a=5;
+// let b=2;
+
+// console.log("a =" ,a,"& b =",b);
+
+// console.log("a--  =", a--);
+// console.log("a =", a); 
+
+// let a = 5;
+// let b = 2;
+
+// console.log("a =" ,a,"& b =",b);
+
+// console.log("a++  =", a++);
+// console.log("a =", a); 
+
+
+// let a = 5;
+// let b = 5;
+// console.log("a == b", a == b);
+
+// let a = 5;
+// let b = 5;
+// console.log("a !== b", a !== b);
+
+// let a = 5;
+// let b = 3;
+// console.log("a < b", a < b);
+
+// let a = 5;
+// let b = 3;
+// console.log("a && b", a && b);
+
+// let a = 6;
+// let b = 5;
+
+// let cond1 = a > b;
+// let cond2 = a === b;
+// console.log("cond1 && cond2", cond1 && cond2);
+
+
+let a = 6;
+let b = 5;
+
+console.log("cond1 || cond2", a < b || a === 6);
+
